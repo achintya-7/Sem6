b9fb6a9c7aaf461af0bb2ce834e2f807428b2a16
achintya <achintya22052000@gmail.com>
2023-01-30 12:03:45 +0000

    push

---
+++ Compilor/L.java
@@ -0,0 +1,13 @@
+public class L {
+    public static void main(String[] args) {
+        L.youFellOf().RatioBuilder();
+    }
+
+    public static void youFellOf() {
+
+    }
+
+    public class RatioBuilder {
+
+    }
+}
